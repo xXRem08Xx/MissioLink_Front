fc6aa3af10997eefe30ddbaa639d3f058c5b2e84
xXRem08Xx <maissa.rem08@gmail.com>
2025-05-31 11:25:58 +0200

    fix deconexion

--- src/app/layout/nav-layout/nav-layout.component.ts
+++ src/app/layout/nav-layout/nav-layout.component.ts
@@ -7,11 +7,12 @@ import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { CommonModule } from '@angular/common';
+import { LogoutComponent } from '../../components/logout/logout.component';
 
 @Component({
   selector: 'app-nav-layout',
   standalone: true,
-  imports: [NzDropDownModule, CommonModule, RouterOutlet, NzLayoutModule, NzMenuModule, NzAvatarModule, NzIconModule],
+  imports: [NzDropDownModule, CommonModule, RouterOutlet, NzLayoutModule, NzMenuModule, NzAvatarModule, NzIconModule, LogoutComponent],
   templateUrl: './nav-layout.component.html',
   styleUrls: ['./nav-layout.component.css']
 })
